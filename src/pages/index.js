87bc386fbd37b6279ff3ed0c04a27e8536a91d21
Loïc Nogues <nogues.loic@gmail.com>
2018-09-05 15:14:20 +0200

    Added more skillz

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,7 +8,6 @@ class IndexPage extends React.Component {
   }
 
   render() {
-    console.log(this.props.size);
     return (
       <div id="index" className={this.state.flip ? 'flip' : ''}>
         <h1>🥖 Loïc Nogues</h1>
@@ -42,7 +41,7 @@ class IndexPage extends React.Component {
         <fieldset>
           <legend>🏆 Skillz</legend>
           <p id="marquee" style={{ width: `${this.props.size.width - 37}px` }}>
-            <span className="first">React ⚡ Redux ⚡ Vue ⚡ Angular.js ⚡ D3.js ⚡ Firebase ⚡ Node.js ⚡ MongoDB ⚡ SQL (MySQL, Postgres) ⚡ Ubuntu ⚡ Vagrant ⚡ Electron.js ⚡ ReactNative</span>
+            <span className="first">React ⚡ Redux ⚡ Vue ⚡ Angular.js ⚡ D3.js ⚡ Firebase ⚡ Node.js ⚡ MongoDB ⚡ MySQL ⚡ Postgres ⚡ Ubuntu ⚡ Vagrant ⚡ Electron.js ⚡ ReactNative ⚡ Gulp ⚡ Webpack ⚡ TypeScript</span>
           </p>
         </fieldset>
         <button onClick={() => this.setState({ flip: !this.state.flip })}>
